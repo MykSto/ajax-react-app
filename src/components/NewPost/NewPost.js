ff7e34fed9d7bd33b82502f487f2550b14278861
Michail <hushokas@gmail.com>
2020-02-22 20:44:11 +0200

    [001]: state fix

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -6,14 +6,14 @@ import './NewPost.css';
 const NewPost = () => {
   const [state, setState] = useState({
     title: '',
-    content: '',
+    body: '',
     author: 'Mike',
   });
 
   const postDataHandler = () => {
     const data = {
       title: state.title,
-      content: state.content,
+      body: state.body,
       author: state.author,
     };
 
@@ -28,12 +28,12 @@ const NewPost = () => {
     <div className="NewPost">
       <h1>Add a Post</h1>
       <label>Title</label>
-      <input type="text" value={state.title} onChange={(event) => setState({ title: event.target.value })} />
+      <input type="text" value={state.title} onChange={(event) => setState({ ...state, title: event.target.value })} />
       <label>Content</label>
-      <textarea rows="4" value={state.body} onChange={(event) => setState({ body: event.target.value })} />
+      <textarea rows="4" value={state.body} onChange={(event) => setState({ ...state, body: event.target.value })} />
       <label>Author</label>
       {state.body}
-      <select value={state.author} onChange={(event) => setState({ author: event.target.value })}>
+      <select value={state.author} onChange={(event) => setState({ ...state, author: event.target.value })}>
         <option value="Michail">Michail</option>
         <option value="Mike">Mike</option>
       </select>
